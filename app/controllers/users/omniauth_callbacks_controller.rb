34b02e5dee0d223330b26ef229f243edf2e7353b
Jay Shirley <j@shirley.im>
2013-12-15 13:06:53 -0800

    Fixing authentication, need to store the provider in the user table.

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -2,11 +2,13 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def github
     @user = User.find_for_github_oauth(request.env["omniauth.auth"], current_user)
 
+    logger.ap @user
+
     if @user.persisted?
       sign_in_and_redirect @user, event: :authentication
       set_flash_message(:notice, :success, kind: "GitHub") if is_navigational_format?
     else
-      session["devise.github_data"] = request.env["omniauth.auth"]
+      session["devise.github_email"] = request.env["omniauth.auth"].info.email
       redirect_to new_user_registration_url
     end
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,12 +7,14 @@ class User < ActiveRecord::Base
 
   def self.find_for_github_oauth(auth, signed_in_resource=nil)
     user = User.where(provider: auth.provider, uid: auth.uid).first
+
     unless user
       user = User.create(
-        name: auth.extra.raw_info.name,
-        uid: auth.uid,
+        name:  auth.extra.raw_info.name,
+        uid:   auth.uid,
         email: auth.info.email,
-        password: Devise.friendly_token[0,20]
+        password: Devise.friendly_token[0,20],
+        provider: auth.provider
       )
     end
     user
