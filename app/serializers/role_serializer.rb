fdd78a562a73c61ef548478c7d38cfdac0d003bd
Cory G Watson <gphat@onemogin.com>
2013-12-24 09:49:55 -0800

    Add needed_count field to serialization for easier ES queries for uncompleted roles.

--- app/serializers/role_serializer.rb
+++ app/serializers/role_serializer.rb
@@ -1,4 +1,8 @@
 class RoleSerializer < ActiveModel::Serializer
-  attributes :id, :name, :filled_count, :quantity
+  attributes :id, :name, :filled_count, :needed_count, :quantity
   has_many :users
+
+  def needed_count
+    return object.quantity - object.filled_count
+  end
 end
